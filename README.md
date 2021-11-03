4432965b9669a137815cf550d4bca0ae9b46e597
daer4n <fuzzebf@gmail.com>
2021-11-03 15:26:04 +0100

    :bento: Ajout du readme et de la bdd

---
+++ README.md
@@ -0,0 +1,6 @@
+Mon site de début d'année avec un formulaire de connexion associé.
+
+Le site a besoin d'une base de donnée dont la requete est donnée dans le fichier bdd.sql
+
+Le site permet uniquement de se créer un compte à l'aide d'une adresse email et d'un mot de passe (le mot de passe est crypté)
+et de se connecter avec des identifiants déjà présent dans la base de donnée.
---
+++ bdd.sql
@@ -0,0 +1,34 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           5.7.33 - MySQL Community Server (GPL)
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour indust_php
+DROP DATABASE IF EXISTS `indust_php`;
+CREATE DATABASE IF NOT EXISTS `indust_php` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+USE `indust_php`;
+
+-- Listage de la structure de la table indust_php. compte
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
+  `mail` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(64) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`mail`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
